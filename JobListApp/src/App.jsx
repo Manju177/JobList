8b38052894c8c62a312c5a58b95cee827c985808
Manju177 pattanashetti <manjunathpattanashetti166@gmail.com>
2024-05-03 04:50:27 -0700

    router added

--- JobListApp/src/App.jsx
+++ JobListApp/src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 import JobList from './Components/JobList/JobList';
+import {BrowserRouter,Route,Routes} from 'react-router-dom' 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
    <div>
-     <JobList/>
+     <BrowserRouter>
+     <Routes>
+      <Route path='/' element={<JobList/>} exact/>
+      {/* <Route path='/xyz' element={<xyz/>} /> */}
+      </Routes>
+     </BrowserRouter>
    </div>
   )
 }
